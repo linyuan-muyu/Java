8dbd66770bb5eb3cd23189855750d2ede98c9596
linyuanmuyu <linyuanmuyu@outlook.com>
2019-11-19 10:08:19 +0800

    第一次上传设置“ exit git commit -m 第一次上传设置“

---
+++ python/python.py
@@ -0,0 +1,9 @@
+print("Hello World!")
+message = "教练，我想学python！"
+print(message)
+message = "教练，我又想学python了。。。"
+print(message)
+message = "教练，我又又想学python了。。。。。。"
+print(message)
+name = "ada lovelace"
+print(name.title())
